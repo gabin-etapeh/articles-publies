b77c592cb366b10fbfd45f9fb37ff918b5c49468
gabin-etapeh <gabietapeh@gmail.com>
2024-06-20 04:04:52 +0100

    ajout d'une page d'affichage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppServiceService } from './app-service.service';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor,NgIf,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  dataFromAPI: any;
+  dataFromAPI: any ;
+  title: any;
 
   constructor(private appService: AppServiceService) { }
 
